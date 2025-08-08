fd1c29d1106dc66b5629f5ccfee3aed5aa4ab490
CaesarLi-WantToBeAFreeMan <krbw0110010000@gmail.com>
2025-08-08 18:43:00 +0800

    initialize frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <router-view/>
+</template>
+
+<script lang = "ts">
+    export default{
+        name: "app"
+    }
+</script>
+
+<style lang = "scss">
+</style>
---
+++ frontend/src/router/index.ts
@@ -0,0 +1,24 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+import Home from "@/views/Home.vue";
+
+const routes: Array <RouteRecordRaw> = [
+    {
+        path: "/",
+        name: "home",
+        component: Home
+    },
+    /*
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/AboutView.vue')
+    }
+    */
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
+
+export default router;
